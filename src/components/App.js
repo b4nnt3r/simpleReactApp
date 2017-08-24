38418e9d0e42ac81fb6736ba319c8c2cff6e221a
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-24 12:46:18 -0400

    moved Info and List into components directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import logo from '../logo.svg'
-import EarthquakeInfo from '../data/EarthquakeInfo.js';
-import EarthquakeList from '../data/EarthquakesList.js';
+import EarthquakeInfo from './EarthquakeInfo';
+import EarthquakeList from './EarthquakesList';
 
 class App extends Component {
   render() {
